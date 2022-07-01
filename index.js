9f0edaf7d1daacbefa6e80064855f96ee39d2689
YuliaYurievna <yuliapoddyacha@gmail.com>
2022-07-01 20:45:42 +0300

    Display the forecast data using the API

--- index.js
+++ index.js
@@ -26,6 +26,57 @@ let dateElement = document.querySelector('#date');
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector('#forecast');
+
+  let forecastHTML = `<div class="row text-center ml-0 mr-0 mt-2 mb-2 shadow">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+        <div class="col-2 card pt-2 pb-2 weather-elements">
+          <div class="weather-forecast-date">
+          ${formatDay(forecastDay.dt)}
+          </div>
+          <div class="forecast-icon mt-1 mb-1">
+            <img src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" alt="" width="44">
+          </div>
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">
+            ${Math.round(forecastDay.temp.max)}°
+            </span>
+            <span class="weather-forecast-temperature-min">
+            ${Math.round(forecastDay.temp.min)}°
+            </span>
+          </div>
+        </div>
+      `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = '98f5a37ff9ffddbb3334ee960c2d442a';
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeatherCondition(response) {
   console.log(response);
   document.querySelector('#city').innerHTML = response.data.name;
@@ -48,6 +99,8 @@ function displayWeatherCondition(response) {
       'src',
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+
+  getForecast(response.data.coord);
 }
 
 function searchedCity(city) {
@@ -93,6 +146,7 @@ function displayFahrenheitTemperature(event) {
 }
 
 let celsiusTemperature = null;
+searchedCity('Kharkiv');
 
 let fahrenheitLink = document.querySelector('#fahrenheit-link');
 fahrenheitLink.addEventListener(
